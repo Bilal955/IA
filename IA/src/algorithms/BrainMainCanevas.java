428d35819d344598a7840b30816efc678f90f414
issathink <issamht1@gmail.com>
2017-02-25 23:57:27 +0100

    Little bit better...

--- IA/src/algorithms/BrainMainCanevas.java
+++ IA/src/algorithms/BrainMainCanevas.java
@@ -20,7 +20,8 @@ public class BrainMainCanevas extends Brain {
 	// ---VARIABLES---//
 	private static final double HEADINGPRECISION = 0.001;
 	private Random rand;
-	private boolean shoot;
+	private int shoot;
+	private int shootEnemy;
 	private boolean front;
 	private boolean turning;
 	private boolean avoid;
@@ -36,7 +37,8 @@ public class BrainMainCanevas extends Brain {
 	public void activate() {
 		move();
 		front = true;
-		shoot = true;
+		shoot = 0;
+		shootEnemy = 0;
 		rand = new Random();
 		rand.setSeed(10);
 		turning = false;
@@ -48,98 +50,135 @@ public class BrainMainCanevas extends Brain {
 		if (isHeading(getHealth()))
 			return;
 
-		boolean nobody = false;
+		boolean nobody = true;
+		IFrontSensorResult.Types frontType = detectFront().getObjectType();
 		ArrayList<IRadarResult> res = detectRadar();
+		if(!frontType.toString().equals("NOTHING"))
+			System.out.println("> " + id + " front: " + frontType + " avance: " + front);
+
 		if (turning) {
-			// System.out.println("Turning angle " + getHeading());
 			stepTurn(Direction.LEFT);
-			if(getHeading() != 0.0 && isEndTurn(Math.PI)) {
+			if (getHeading() != 0.0 && isEndTurn(Math.PI)) {
 				System.out.println("Stop turning");
 				turning = false;
+				if(rand.nextInt(5) == 1)
+					avoid = true;
 			}
 			return;
 		}
-		
-		if(avoid) {
-			System.out.println("Je evite");
+
+		if (avoid) {
 			stepTurn(Direction.LEFT);
-			if(getHeading() != 0.0 && isEndTurn(Math.PI/2)) {
-				System.out.println("Arreter de tourner");
+			if (getHeading() != 0.0 && isEndTurn(Math.PI / 2)) {
+				System.out.println(">" + id + " arretes de tourner");
 				avoid = false;
 			}
 			return;
 		}
-		
-		if(detectFront().getObjectType() == IFrontSensorResult.Types.BULLET) {
-			System.out.println("Je recule y a une balle");
+
+		if (frontType == IFrontSensorResult.Types.Wreck) {
+			moveBack();
+			avoid = true;
+			return;
+		}
+
+		int nbEnemy = 0;
+		for (IRadarResult iRadarResult : res) {
+			IRadarResult.Types type = iRadarResult.getObjectType();
+			if (type == IRadarResult.Types.OpponentMainBot || type == IRadarResult.Types.OpponentSecondaryBot) {
+				nbEnemy++;
+				nobody = false;
+			}
+		}
+		if (nbEnemy >= 2) {
 			moveBack();
 			return;
 		}
 
 		for (IRadarResult iRadarResult : res) {
 			IRadarResult.Types type = iRadarResult.getObjectType();
-			
-			if(type == IRadarResult.Types.Wreck && isHeading(iRadarResult.getObjectDirection())) {
-				System.out.println("J'essaye d'eviter quelqu'un + " + id);
-				// stepTurn(Direction.LEFT);
+
+			if (type == IRadarResult.Types.Wreck && isHeading(iRadarResult.getObjectDirection())) {
+				System.out.println(">" + id + "j 'essaye d'eviter WRECK");
 				moveBack();
-				// front = false;
 				avoid = true;
 				return;
 			}
 
-			// Si je vois un ennemi je lui tire dessus
+			// Si je vois un ennemi je lui tire dessus tout en reculant
 			if (type == IRadarResult.Types.OpponentMainBot || type == IRadarResult.Types.OpponentSecondaryBot) {
-				fire(iRadarResult.getObjectDirection());
-				// System.out.println("Tirer vers " + iRadarResult.getObjectType().toString());
-				// front = false;
+				if (shootEnemy % 4 == 0)
+					moveBack();
+				else
+					fire(iRadarResult.getObjectDirection());
+				shootEnemy++;
 				return;
 			}
+
 			// S'il y a une balle en ma direction je tire aussi
-			// double objDirection = iRadarResult.getObjectDirection();
-			// if (type == IRadarResult.Types.BULLET && isHeading(objDirection))
-			// {
-			// fire(objDirection);
-			// return;
-			// }
-			// System.out.println(iRadarResult.getObjectType());
+			double objDirection = iRadarResult.getObjectDirection();
+			if (type == IRadarResult.Types.BULLET && isHeading(objDirection)) {
+				if (shoot % 3 == 0)
+					moveBack();
+				else
+					fire(objDirection);
+				// if (rand.nextBoolean())
+				// avoid = true;
+				shoot++;
+				return;
+			}
 		}
 
 		// Si je vois personne à l'horizon je bouge
-		if(nobody)
+		if (nobody)
 			front = true;
 
 		// S'il y a un mur je recule
-		if (detectFront().getObjectType() == IFrontSensorResult.Types.WALL) {
+		if (frontType == IFrontSensorResult.Types.WALL) {
 			turning = true;
+		} else if (frontType == IFrontSensorResult.Types.TeamMainBot
+				|| frontType == IFrontSensorResult.Types.TeamSecondaryBot) {
+			System.out.println("> " + id + " front: " + frontType);
+			avoid = true;
+			shoot = 1; // pas tirer sur ses partenaires
 		}
 
 		// Soit je tire soit je bouge
-		if (shoot) {
-			// System.out.println("Je tire vers " + getShootingAngle());
-			fire(getShootingAngle());
-			shoot = false;
+		if (shoot % 3 == 0) {
+			// fire(getShootingAngle());
+			move();
 		} else {
-			if (front) {
-				// System.out.println("J'avance mais wtf");
+			if (front)
 				move();
-			} else {
-				// System.out.println("Y a un mur devant je dois reculer");
+			else
 				moveBack();
-			}
-			shoot = true;
 		}
+		shoot++;
 	}
 
 	public double getShootingAngle() {
-		return rand.nextDouble() < 0.5 ? rand.nextDouble() + getHeading() : getHeading() - rand.nextDouble();
+		double angle = rand.nextDouble() < 0.5 ? rand.nextDouble() + getHeading() : getHeading() - rand.nextDouble();
+		ArrayList<IRadarResult> res = detectRadar();
+		for (IRadarResult e : res) {
+			IRadarResult.Types type = e.getObjectType();
+
+			if (type == IRadarResult.Types.TeamMainBot || type == IRadarResult.Types.TeamSecondaryBot)
+				if (isHeading(e.getObjectDirection(), angle))
+					angle = angle + Math.PI / 8;
+		}
+		return angle;
 	}
 
 	private boolean isHeading(double dir) {
 		return Math.abs(Math.sin(getHeading() - dir)) < HEADINGPRECISION;
 	}
-	
+
+	private boolean isHeading(double dir, double heading) {
+		return Math.abs(Math.sin(heading - dir)) < HEADINGPRECISION;
+	}
+
 	public boolean isEndTurn(double val) {
-		return Math.abs(Math.sin((getHeading()%val) - val)) < HEADINGPRECISION;
+		return Math.abs(Math.sin((getHeading() % val) - val)) < HEADINGPRECISION
+				|| Math.abs(Math.sin(getHeading() % val)) < HEADINGPRECISION;
 	}
 }
